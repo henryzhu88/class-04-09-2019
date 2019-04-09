30ce325b07863deb3bbde9ba93ba4292fbb94705
Henry Zhu <henryzhu@Henrys-MacBook-Pro-711.local>
2019-04-09 13:55:44 -0400

    first commit

---
+++ notebook.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(readr)
+
+x1 <- read_csv("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-nj03-1.csv")
+
+x2 <- read_csv("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-nj03-3.csv")
+                                    
+x3 <- read_csv("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-nj07-1.csv")
+
+x4 <- read_csv("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-nj07-3.csv")
+
+x5 <- read_csv("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-nj11-3.csv")
